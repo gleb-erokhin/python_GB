f87a225960f97717e44718d16b2ce47150a6669c
Gleb <gleb.erokhin@gmail.com>
2021-10-06 18:10:26 +0300

    first step 06/10/21

---
+++ task_1.py
@@ -0,0 +1,15 @@
+# Task 1
+duration = int(input('Введите время в секундах: '))
+
+# if duration < 60:
+#     print(duration, 'сек')
+# elif duration > 60:
+#     days = duration // 86400
+#     hours = duration % 3600
+#     print(days, hours)
+# days = duration // 24 * 60 * 60
+# print(days)
+# hour = (duration - days) /
+# print(hour)
+
+
---
+++ task_3.py
@@ -0,0 +1,13 @@
+# percent = int(input('Введите значение %: '))
+# if percent <= 1:
+#     print(percent, 'процент')
+# elif percent <= 4:
+#     print(percent, 'процента')
+# elif percent >= 5:
+#     print(percent, 'процентов')
+
+numbers = [1, 21, 31, 41]
+for number in numbers:
+    print(number)
+    # print(a[i] // 3)
+    # i += 1
