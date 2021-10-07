2f2b0c388b013c6aa8d6d9f147d449f20784ea21
Gleb <gleb.erokhin@gmail.com>
2021-10-07 17:40:43 +0300

    second day

--- task_1.py
+++ task_1.py
@@ -1,15 +1,18 @@
 # Task 1
 duration = int(input('Введите время в секундах: '))
+# тут я пробовал сделать это с помощью цикла, но все равно не пойму смысл именно
+# каким образом вообще отделять часы минуты, на основании какого принципе это делатся
+# почему что то делать через % а что то через //. целочисленные и с остатком в чем разница.
+if duration < 60:
+    print(duration, 'сек')
+elif duration > 60:
+    days = duration // 86400
+    hours = duration % 3600
+    print(days, hours)
 
-# if duration < 60:
-#     print(duration, 'сек')
-# elif duration > 60:
-#     days = duration // 86400
-#     hours = duration % 3600
-#     print(days, hours)
-# days = duration // 24 * 60 * 60
-# print(days)
-# hour = (duration - days) /
-# print(hour)
+days = duration // 24 * 60 * 60
+print(days)
+hour = (duration - days) /
+print(hour)
 
 
