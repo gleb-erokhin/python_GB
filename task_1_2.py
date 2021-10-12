6a979696fbbe4d1988add5991949f8e997e48d78
Gleb <gleb.erokhin@gmail.com>
2021-10-12 16:09:43 +0300

    all tasks from lesson_1 is done

--- task_1_2.py
+++ task_1_2.py
@@ -1,12 +1,30 @@
-# my_list = list(range(1, 50, 2))  # создаем последовательность от 1 до 1000
-# print(my_list)
-# my_list2 = [i**3 for i in my_list]
-# print(my_list2)
+my_list = list(range(1, 1001, 2))  # создаем последовательность от 1 до 1000
+my_list2 = [i ** 3 for i in my_list] # создадим кубы из элементов списков
 
-num = int(input("Введите целое: "))
-sum = 0
-while num != 0:
-    sum = sum + num % 10
-    num = num // 10
-print("Сумма цифр числа равна: ", sum)
+final_sum = 0
+for i in my_list2:      # перебираем последовательность списка
+    my_str = str(i)     # каждый элемент списка переводим в строку
+    my_lst = list(my_str)    # каждый элемент строки переводим в список
+    full_sum = [int(item) for item in my_lst]  # создаем сумму всех чисел в выводе каждого элемента куба
+    if sum(full_sum) % 7 == 0:     # проверяем делится ли сумма на 7 нацело
+        final_sum += i
+print(final_sum)
+
+
+# попробованные варианты ранее
+# print(len(my_list2))
+# print(check_list.append(3333))
+# print(type(check_list))
+# print(check_list.pop())
+# print(check_list)
+
+# for i in my_list2:
+#     my_str = str(i)
+#     print(my_str)
+
+# for i in my_list2:
+#     my_list3.append(i)
+# print(my_list3)
+
+
 
--- task_1_3.py
+++ task_1_3.py
@@ -1,10 +1,23 @@
 percent = int(input('Введите значение %: '))
-one = [2, 3, 4]
-if percent == 1:
-    print(percent, 'процент')
-elif percent <= 4:
+if 5 <= percent % 100 <= 20:
+    print(percent, 'процентов')
+elif 2 <= percent % 10 <= 4:
     print(percent, 'процента')
-elif percent >= 5:
+elif percent % 10 == 1:
+    print(percent, 'процент')
+else:
     print(percent, 'процентов')
 
-print(25 % 4)
+# ///////////////////////////////////
+
+# percent = 0
+# while percent < 100:             # с использованием цикла, прописывает все цифры от 1 до 100
+#     percent = percent + 1
+#     if 5 <= percent % 100 <= 20:
+#         print(percent, 'процентов')
+#     elif 2 <= percent % 10 <= 4:
+#         print(percent, 'процента')
+#     elif percent % 10 == 1:
+#         print(percent, 'процент')
+#     else:
+#         print(percent, 'процентов')
