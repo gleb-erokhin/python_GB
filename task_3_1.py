c10857cf24c4f6b72865c4fbd2ca45bee75a609e
gleb-erokhin <gleb.erokhin@gmail.com>
2021-10-22 16:22:39 +0300

    update

---
+++ task_3_1.py
@@ -0,0 +1,24 @@
+# def num_translate():
+#     pass
+#
+#
+# word_list = {
+#     'one': 'один',
+#     'two': 'два',
+#     'tree': 'три',
+#     'four': 'четыре',
+#     'five': 'пять',
+#     'six': 'шесть',
+#     'seven': 'семь',
+#     'eight': 'восемь',
+#     'night': 'девять',
+#     'ten': 'десять',
+# }
+#
+# print(word_list['ten'])
+# print(word_list.get('eleven'))
+
+ # задача сделать резюме билдер
+
+
+
