db7b74a0a95a1aa36c6c16a441c42480393d3fc5
gleb-erokhin <gleb.erokhin@gmail.com>
2021-10-26 06:12:45 +0300

    1 and 2 task done

--- task_3_1.py
+++ task_3_1.py
@@ -1,24 +1,21 @@
-# def num_translate():
-#     pass
-#
-#
-# word_list = {
-#     'one': 'один',
-#     'two': 'два',
-#     'tree': 'три',
-#     'four': 'четыре',
-#     'five': 'пять',
-#     'six': 'шесть',
-#     'seven': 'семь',
-#     'eight': 'восемь',
-#     'night': 'девять',
-#     'ten': 'десять',
-# }
-#
-# print(word_list['ten'])
-# print(word_list.get('eleven'))
-
- # задача сделать резюме билдер
+def num_translate(key):
+    # запрос get для работы со словарем по ключу,
+    # так же указываем параметр если не будет слова в словаречтобы не вызвать ошибку
+    return print(word_list.get(key, 'Такого числа нет'))
 
 
+word_list = {
+    'one': 'один',
+    'two': 'два',
+    'tree': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'night': 'девять',
+    'ten': 'десять',
+}
 
+num_translate('One')
+num_translate('one')
---
+++ task_3_2.py
@@ -0,0 +1,27 @@
+def num_translate_adv(key):
+    # создаем переменную для сброса регистра в нижний
+    # xnj,s ghjdthbnm
+    word = word_list.get(key.lower(), 'Такого числа нет')
+    # циклом проверяем по индексу первая буква слова большая или маленькая
+    # затем возвращаем нужный результат
+    if key[0].isupper():
+        return word.capitalize()
+    else:
+        return word
+
+
+word_list = {
+    'one': 'один',
+    'two': 'два',
+    'tree': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'night': 'девять',
+    'ten': 'десять',
+}
+
+a = num_translate_adv('Six')
+print(a)
