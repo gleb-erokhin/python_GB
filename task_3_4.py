d2c8af76aef21897acae8c461e1b6f7475d9ab76
Gleb <gleb.erokhin@gmail.com>
2021-10-29 17:27:26 +0300

    update 4, 5

--- task_3_4.py
+++ task_3_4.py
@@ -1,14 +1,16 @@
-def thesaurus_adv(*names):
+def thesaurus_adv(*names_surnames):
     res = {}
     print(res, 'сделали словарь имя')
-    for name in names:
-        key = name[0].capitalize()
-        name, surname = name.split()
-        res.setdefault(surname, {})
-        res[surname[0]].setdefault(key, [])
-        res[surname[0]][key].append(name)
+    for name_surname in names_surnames:
+        name, surname = name_surname.split()
+        name = name[0].capitalize()
+        surname = surname[0].capitalize()
+        res.setdefault(surname[0], {})
+        res[surname[0]][name[0]] = []
+        res[surname[0]][name[0]].append(name_surname)
     return res
 
 
-tera = thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
+tera = thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева", "Глеб Ерохин", \
+                     "Глеб Ерохин1")
 print(tera)
