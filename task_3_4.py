45ae787a38f5ce28100bbbcb20dbde31b1829c13
gleb-erokhin <gleb.erokhin@gmail.com>
2021-10-29 19:40:28 +0300

    home

--- task_3_4.py
+++ task_3_4.py
@@ -3,12 +3,18 @@ def thesaurus_adv(*names):
     print(res, 'сделали словарь имя')
     for name in names:
         key = name[0].capitalize()
+        print(key, 'создаем ключ первая буква')
         name, surname = name.split()
-        res.setdefault(surname, {})
-        res[surname[0]].setdefault(key, [])
-        res[surname[0]][key].append(name)
+        print(name, surname, 'делим на имя и фамилию')
+        res.setdefault(surname[0], {})
+        # print(res, 'добавляем в словарь')
+        # res[name[0]].setdefault(key, [])
+        # res[surname[0]][key].append(name)
     return res
 
 
-tera = thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
+
+tera = thesaurus_adv("Иван Сергеев")
 print(tera)
+
+# ", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева
