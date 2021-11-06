a3fe94b8bcec730e2d95697e880d75b8953b28b9
gleb-erokhin <gleb.erokhin@gmail.com>
2021-11-06 14:50:35 +0300

    all dz is done

--- task_3_4.py
+++ task_3_4.py
@@ -1,12 +1,12 @@
 def thesaurus_adv(*names_surnames):
     res = {}
     for name_surname in names_surnames:
-        name, surname = name_surname.split()
-        name = name[0].capitalize()
-        surname = surname[0].capitalize()
-        res.setdefault(surname[0], {})
-        res[surname[0]][name[0]] = []
-        res[surname[0]][name[0]].append(name_surname)
+        name, surname = name_surname.split()  # разделяем имя и фамиляю для определения буквы имени и фамилии
+        name = name[0].capitalize()  # буква имени в верхний регистр
+        surname = surname[0].capitalize()  # буква фамилии в верхний регистр
+        res.setdefault(surname[0], {})  # наполняем словарь по первой буквы фамилии
+        res[surname[0]][name[0]] = []  # создаем список внутри словаря
+        res[surname[0]][name[0]].append(name_surname)  # добавляем в список данные
     return res
 
 
