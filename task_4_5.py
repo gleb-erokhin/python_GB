41e80d233e19e13bb911100370ade706395bb223
Gleb Erokhin <89621836+gleb-erokhin@users.noreply.github.com>
2021-11-08 16:03:50 +0300

    task_4_5 final version

--- task_4_5.py
+++ task_4_5.py
@@ -3,7 +3,5 @@ import sys
 
 
 money = sys.argv[1]  # передаем в параметр для типа валюты для ввода в командной строке
-if isinstance(money, str):
-    print(task_4_3.currency_rates_d(money))  # обращение к функции через модуль task_4_3.py
-else:
-    print('Ошибка ввода')
+print(task_4_3.currency_rates_d(money))  # обращение к функции через модуль task_4_3.py
+
