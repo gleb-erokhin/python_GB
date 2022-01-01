7f9dff7474976292f151b671be3195dd7d7e6ead
gleb-erokhin <gleb.erokhin@gmail.com>
2022-01-01 15:10:01 +0300

    task 6.2 final v2

--- task_6.2.py
+++ task_6.2.py
@@ -16,10 +16,10 @@ from itertools import count
 
 f_obj = open('nginx_logs.txt', 'r', encoding='utf-8')
 my_l = []
-unique = []
 for line in f_obj:
     str_my = line.split()
     my_l.append(str_my[0])
 for i in my_l:
     curr_frequency = my_l.count(i)
 print(curr_frequency, i)
+f_obj.close()
